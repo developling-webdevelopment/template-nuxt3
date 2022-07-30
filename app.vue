d651365ede53011e413ba275d4c1c54e1731b4be
Ling <jeroen@smit.net>
2022-07-30 19:59:26 +0200

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <Html :lang="'nl'">
+    <Body
+      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
+    >
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+import { defineNuxtConfig } from 'nuxt'
+import UnpluginComponentsVite from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  buildModules: ['@nuxtjs/eslint-module','unplugin-icons/nuxt'],
+  modules: ['@nuxtjs/tailwindcss','@nuxt/content','@vueuse/nuxt'],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
+   app: {
+    head: {
+      title: 'Nuxt 3 Awesome Starter',
+      titleTemplate: '%s - Nuxt 3 Awesome Starter',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Nuxt 3 Awesome Starter',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  typescript: {
+    shim: false
+  },
+   vite: {
+    plugins: [
+      UnpluginComponentsVite({
+        dts: true,
+        resolvers: [
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+      }),
+    ],
+  },
+})
